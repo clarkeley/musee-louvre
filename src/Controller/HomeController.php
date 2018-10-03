a07b74bf3156eb98c4cc4e9c27e8fa3e87dd4a9f
Emmanuel <clarke_ley@live.fr>
2018-10-03 19:34:56 +0200

    Initial commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+	namespace App\Controller;
+
+	use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\HttpFoundation\Response;
+	use Symfony\Component\Routing\Annotation\Route;
+	use Twig\Environment;
+
+	/**
+	 * @Route("/", name="home")
+	 */
+	class HomeController extends Controller
+	{
+		private $twig;
+
+		public fuction __construct(Environment $twig)
+		{
+			$this->twig = $twig;
+		}
+
+		return new Response($this->twig->render('home.html.twig'));
+	}
