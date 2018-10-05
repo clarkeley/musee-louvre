d2d8eddc768cb6ee213aa3338454f4f9c196bb3b
Emmanuel <clarke_ley@live.fr>
2018-10-05 14:01:03 +0200

    add form contact

---
+++ src/Form/ContactType.php
@@ -0,0 +1,37 @@
+<?php
+
+    namespace App\Form;
+
+    use App\Entity\Contact;
+    use Symfony\Component\Form\AbstractType;
+    use Symfony\Component\Form\FormBuilderInterface;
+    use Symfony\Component\OptionsResolver\OptionsResolver;
+    use Symfony\Component\Form\Extension\Core\Type\EmailType;
+    use Symfony\Component\Form\Extension\Core\Type\TextType;
+    use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+    use Symfony\Component\Form\Extension\Core\Type\EmailType;
+    use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+    use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+
+    class ContactType extends AbstractType
+    {
+        public function buildForm(FormBuilderInterface $builder, array $options)
+        {
+            $builder
+                ->add('username', TextType::class)
+                ->add('message', TextareaType::class)
+                ->add('from', RepeatedType::class, array(
+                    'type' => EmailType::class,
+                    'first' => array('label', => 'Email'),
+                    'second' => array('label', => 'Repeat Email'),
+                ))
+            ;
+        }
+
+        public function configureOptions(OptionsResolver $resolver)
+        {
+            $resolver->setDefaults(array(
+                'data_class' => Contact::class,
+            ));
+        }
+    }
