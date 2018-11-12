d3f5ff6ef15ac39dbc053821889a6e9e56ae2217
Emmanuel <clarke_ley@live.fr>
2018-11-12 21:19:35 +0100

    Bootstrap modifications, contactForm -> sendmail

--- src/Form/FormHandler/ContactTypeHandler.php
+++ src/Form/FormHandler/ContactTypeHandler.php
@@ -8,37 +8,23 @@
       final class ContactTypeHandler
       {
 
-      	public function handle(FormInterface $form) : bool
+      	public function handle(FormInterface $form) : bool //\Swift_Mailer $mailer
       	{
       		if ($form->isSubmitted() && $form->isValid()) {
 
                   /*$builder = $form->getData();
-                  $mailer = new Swift_Mailer($transport);
-                  $transport
-
-                  $builder = new \Swift_Message::newInstance();
-                  $this->get('mailer')->send($builder);
 
                   $message = (new \Swift_Message('ContactMail'))
                         ->setSubject('Contact Form', form.username)
                         ->setFrom(form.email)
                         ->setBody(form.message)
 
-                  //$result = $mailer->send($message);
+                  $mailer->send($message);
 
-                  //$request->getSession()->getFlashBag()->add('notice', 'Form has been sent');
-
-                  //return true;
-                  //$form->getData();*/
-
-                 // $builder = new \Swift_Message::newInstance();
-                  $this->get('mailer')->send($builder);
-
-                  $request->getSession()->getFlashBag()->add('notice', 'Form has been sent');
-
+                  $request->getSession()->getFlashBag()->add('notice', 'Form has been sent');*/
 
                   return true;
-              }
+                  }
 
       		return false;
       	}
