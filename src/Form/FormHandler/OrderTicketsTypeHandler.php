6b69bead0ea9c3aaa85e1e6c05bde2496c8bb293
Emmanuel <clarke_ley@live.fr>
2019-01-10 12:03:24 +0100

    add //date, ifelse logic

--- src/Form/FormHandler/OrderTicketsTypeHandler.php
+++ src/Form/FormHandler/OrderTicketsTypeHandler.php
@@ -10,17 +10,43 @@ namespace App\Form\FormHandler;
 
 
 
+use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Component\Validator\Constraints\DateTime;
 
 class OrderTicketsTypeHandler
 {
+    /**
+     * @var SessionInterface
+     */
+    private $session;
+
+    /* public $today = dateTime();
+    public $today = $dateTime->format("yyyy-MM-dd");
+    public $dateTime = \DateTime::createFromFormat("yyyy-MM-dd", );*/
+
+
+    public function __construct(SessionInterface $session)
+    {
+        $this->session = $session;
+    }
 
     public function handle(FormInterface $form) : bool
     {
         if ($form->isSubmitted() && $form->isValid()) {
             $order = $form->getData();
+            $tickets = $form->getData();
 
-           // TODO mettre à jour les prix age, date typebillet, rate
+            $this->session->set('order', $order);
+            $this->session->set('tickets', $tickets);
+
+           if ($tickets->getBirthday() >= "2001-01-10" & $tickets->getRate() === true or $tickets->getBirthday() < "2001-01-10" & $tickets->getRate() === true) {
+               $tickets->setPrice();
+           }
+           elseif ($tickets->getBirthday() >= "2001-01-10" & $tickets->getRate() === false or $tickets->getBirthday() < "2001-01-10" & $tickets->getRate() === false){
+               $tickets->setPrice();
+           }
 
 
 
