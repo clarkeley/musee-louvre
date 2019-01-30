f47791b25ff43ef5e396d58b72fa6ffa3863274b
Emmanuel <clarke_ley@live.fr>
2019-01-30 22:54:43 +0100

    Add stripe paiements(sucess)

--- src/Controller/BasketController.php
+++ src/Controller/BasketController.php
@@ -25,6 +25,26 @@ class BasketController extends Controller{
 
     public function __invoke(Request $request): Response
     {
+        if ($request->isMethod('POST')) {
+
+            $order = $this->session->get('order');
+
+            $token = $request->request->get('stripeToken');
+
+            \Stripe\Stripe::setApiKey("sk_test_A20bVsterAEgNtzP7zMp1L43");
+            \Stripe\Charge::create(array(
+                "amount" => $order->getTotalPrice() * 100,
+                "currency" => 'eur',
+                "source" => $token,
+                "description" => "Stripe paiement tests"
+            ));
+
+            $this->addFlash('sucess', 'Order Complete !');
+
+            return $this->redirectToRoute('home');
+        }
+
         return $this->render('Shop/shopBasket.html.twig',['order'=>$this->session->get('order')]);
     }
+
 }
