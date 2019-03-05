c8934ac6e93647743c5aefd9bea16e55f04adc53
Emmanuel <clarke_ley@live.fr>
2019-03-05 23:24:24 +0100

    Phpunit testing and front modifs

---
+++ tests/TotoTest.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Clark
+ * Date: 27/02/2019
+ * Time: 14:30
+ */
+
+namespace App\Tests;
+
+
+use PHPUnit\Framework\TestCase;
+
+class TotoTest extends TestCase
+{
+    public function testTrue()
+    {$this->assertTrue(true);
+  }
+}
